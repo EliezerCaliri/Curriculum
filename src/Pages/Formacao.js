a7a6a6fd0167f361a343db4d5052e52286a07662
Eliezer Caliri <yngrydaryane8@gmail.com>
2023-04-12 22:33:29 -0300

    Native

---
+++ src/Pages/Formacao.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Text,View} from 'react-native';
+
+const Formacao = () =>{
+    return (
+        <view>O calvo é o poder</view>
+    );
+}
+
+export default Formacao;
